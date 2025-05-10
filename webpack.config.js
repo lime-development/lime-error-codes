0397b873d8a2c40c55844d28aa84ca8cf1722648
VorobevSA <sv@vorobevsa.com>
2025-05-09 22:30:25 -0300

    Add support ESM, UMD

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,48 @@
 const path = require('path');
 
-module.exports = {
-    entry: './getErrorMessage.mjs',
-    output: {
-        filename: 'getErrorMessage.mjs',
-        path: path.resolve(__dirname, 'dist'),
-        library: '@lime-development/lime-errors-codes',
-        libraryTarget: 'umd',
-    },
-    mode: 'production',
+const commonConfig = {
+  mode: 'production',
+  entry: './getErrorMessage.mjs',
+  resolve: {
+    extensions: ['.mjs', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.json$/,
+        type: 'json',
+      },
+    ],
+  },
 };
+
+const umdConfig = {
+  ...commonConfig,
+  target: 'web',
+  output: {
+    filename: 'getErrorMessage.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      name: '@lime-development/lime-errors-codes',
+      type: 'umd',
+    },
+    globalObject: 'this',
+  },
+};
+
+const esmConfig = {
+  ...commonConfig,
+  target: 'web',
+  output: {
+    filename: 'getErrorMessage.mjs',
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      type: 'module',
+    },
+  },
+  experiments: {
+    outputModule: true,
+  },
+};
+
+module.exports = [umdConfig, esmConfig];
